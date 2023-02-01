a0e7ac58f549118e4d8b9bc06488e49dae9e49b3
ThanhTigi <118548931+ThanhTigi@users.noreply.github.com>
2023-02-01 22:48:56 +0700

    Add files via upload

---
+++ CTDL_005 - XÓA DỮ LIỆU TRONG DSLK ĐƠN.cpp
@@ -0,0 +1,24 @@
+// DSA_PTIT By Thanh TiGi
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main() {
+    int n;
+    cin >> n;
+    int a[n];
+    for (int i=0; i<n; i++)
+	{
+		cin >> a[i];
+	}
+	int k;
+	cin >> k;
+	for (int i=0; i<n; i++)
+	{
+		if (a[i] != k)
+		{
+			cout << a[i] << " ";
+		}
+	}
+    return 0;
+}
