b588de509df1afe7a35e66eb29f82867e5710ca3
ThanhTigi <118548931+ThanhTigi@users.noreply.github.com>
2023-02-04 13:33:06 +0700

    Add files via upload

---
+++ DSA06006 - SẮP XẾP [0 1 2].cpp
@@ -0,0 +1,26 @@
+// DSA_PTIT By Thanh TiGi
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        int a[n];
+        for (int i=0; i<n; i++)
+        {
+            cin >> a[i];
+        }
+        sort(a,a+n);
+        for (auto x : a)
+        {
+            cout << x << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
