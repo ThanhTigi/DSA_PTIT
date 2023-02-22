4f7a2f17cb137692ae5f30cb68ecedfaee89455a
Thành <118548931+ThanhTigi@users.noreply.github.com>
2023-02-23 00:57:14 +0700

    Add files via upload

---
+++ DSA03024 - LỰA CHỌN TỐI ƯU.cpp
@@ -0,0 +1,41 @@
+// DSA_PTIT By Thanh TiGi
+#include<bits/stdc++.h>
+using namespace std;
+struct cv
+{
+    int bg;
+    int ed;
+};
+bool cmp(cv a, cv b)
+{
+    if (a.ed == b.ed) return a.bg < b.bg;
+    return a.ed < b.ed;
+}
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        cv congviec[n];
+        for (int i=0; i<n; i++)
+        {
+            cin >> congviec[i].bg >> congviec[i].ed;
+        }
+        sort(congviec,congviec + n,cmp);
+        int k=1,i=0;
+        for (int j=1; j<n; j++)
+        {
+            if (congviec[j].bg >= congviec[i].ed)
+            {
+                k++;
+                i=j;
+            }
+        }
+        cout << k << endl;
+    }
+    return 0;
+}
