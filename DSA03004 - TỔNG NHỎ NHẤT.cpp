0c5a0823964c2422e953521e24abf70361967589
Thành <118548931+ThanhTigi@users.noreply.github.com>
2023-03-03 19:40:54 +0700

    Add files via upload

---
+++ DSA03004 - TỔNG NHỎ NHẤT.cpp
@@ -0,0 +1,36 @@
+// DSA_PTIT By Thanh TiGi
+#include <bits/stdc++.h>
+using namespace std;
+
+void testCase()
+{
+    int n;
+    cin >> n;
+    int a[n];
+    for (int i = 0; i < n; ++i)
+    {
+        cin >> a[i];
+    }
+    sort(a, a + n);
+    long long x = 0, y = 0;
+    for (int i = 0; i < n; i++)
+    {
+        x = x * 10 + a[i];
+        i++;
+        if (i != n)
+            y = y * 10 + a[i];
+    }
+    cout << x + y;
+}
+
+int main()
+{
+    int T = 1;
+    cin >> T;
+    while (T--)
+    {
+        testCase();
+        cout << "\n";
+    }
+    return 0;
+}
