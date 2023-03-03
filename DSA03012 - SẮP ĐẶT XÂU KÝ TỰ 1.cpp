17d4efd537487fe46e814449ebaf12687b3c6d27
Thành <118548931+ThanhTigi@users.noreply.github.com>
2023-03-03 20:02:28 +0700

    Add files via upload

---
+++ DSA03012 - SẮP ĐẶT XÂU KÝ TỰ 1.cpp
@@ -0,0 +1,32 @@
+// DSA_PTIT By Thanh TiGi
+#include <bits/stdc++.h>
+using namespace std;
+
+void testCase()
+{
+    string s;
+    cin >> s;
+    int dd[127] = {};
+    int maxx = 0;
+    for (char i : s)
+    {
+        dd[i]++;
+        maxx = max(maxx, dd[i]);
+    }
+    if (maxx <= (s.length() + 1) / 2)
+        cout << 1;
+    else
+        cout << -1;
+}
+
+int main()
+{
+    int T = 1;
+    cin >> T;
+    while (T--)
+    {
+        testCase();
+        cout << "\n";
+    }
+    return 0;
+}
