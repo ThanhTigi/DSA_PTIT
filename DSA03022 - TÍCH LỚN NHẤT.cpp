d59d5f6454284017a1125b2356d037f89bc0986b
Thành <118548931+ThanhTigi@users.noreply.github.com>
2023-03-04 13:07:04 +0700

    Add files via upload

---
+++ DSA03022 - TÍCH LỚN NHẤT.cpp
@@ -0,0 +1,31 @@
+// DSA_PTIT By Thanh TiGi
+#include <bits/stdc++.h>
+using namespace std;
+
+void testCase()
+{
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for (int &i : a)
+        cin >> i;
+    sort(a.begin(), a.end());
+    vector<int> res;
+    res.push_back(a[0] * a[1]);
+    res.push_back(a[n - 1] * a[n - 2]);
+    res.push_back(a[0] * a[1] * a[n - 1]);
+    res.push_back(a[n - 1] * a[n - 2] * a[n - 3]);
+    sort(res.rbegin(), res.rend());
+    cout << res[0];
+}
+
+int main()
+{
+    int T = 1;
+    while (T--)
+    {
+        testCase();
+        cout << "\n";
+    }
+    return 0;
+}
