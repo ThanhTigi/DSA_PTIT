36963bd71730c77fb850990a6d76585431c7e064
Thành <118548931+ThanhTigi@users.noreply.github.com>
2023-03-10 20:43:40 +0700

    Add files via upload

---
+++ DSA05005 - SỐ BƯỚC ÍT NHẤT.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void testCase()
+{
+	int n;
+	cin >> n;
+	int a[n], f[n];
+	for (int i = 0; i < n; ++i)
+	{
+		cin >> a[i];
+		f[i] = 1;
+		for (int j = 0; j < i; ++j)
+		{
+			if (a[i] >= a[j])
+			{
+				//! bài này yêu cầu là sort nên chỗ này lấy cả dấu =
+				f[i] = max(f[i], f[j] + 1);
+			}
+		}
+	}
+	cout << n - *max_element(f, f + n);
+}
+
+int main()
+{
+	int T = 1;
+	cin >> T;
+	while (T--)
+	{
+		testCase();
+		cout << "\n";
+	}
+	return 0;
+}
