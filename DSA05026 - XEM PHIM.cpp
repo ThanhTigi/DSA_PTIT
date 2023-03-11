233de7a7fa071cb34af07c5ff930008668e39f77
Thành <118548931+ThanhTigi@users.noreply.github.com>
2023-03-11 21:14:14 +0700

    Add files via upload

---
+++ DSA05026 - XEM PHIM.cpp
@@ -0,0 +1,26 @@
+// DSA_PTIT By Thanh TiGi
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int c, n, w;
+    cin >> c >> n;
+    int f[c + 1] = {};
+    f[0] = 1;
+    for (int i = 1; i <= n; ++i) {
+        cin >> w;
+        for (int j = c; j >= w; --j) {
+            if (f[j] == 0 && f[j - w] == 1) {
+                f[j] = 1;
+            }
+        }
+    }
+    for (int i = c; i >= 0; --i) {
+        if (f[i] == 1) {
+            cout << i << endl;
+            return 0;
+        }
+    }
+    return 0;
+}
