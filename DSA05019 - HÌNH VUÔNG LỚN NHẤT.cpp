113d14feda36ce518b9cde90fa145086d2ede245
Thành <118548931+ThanhTigi@users.noreply.github.com>
2023-03-11 19:42:02 +0700

    Add files via upload

---
+++ DSA05019 - HÌNH VUÔNG LỚN NHẤT.cpp
@@ -0,0 +1,38 @@
+// DSA_PTIT By Thanh TiGi
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n,m; 
+        cin >> n >> m;
+        int a[501][501];
+        for (int i=1; i<=n; i++)
+        {
+            for (int j=1; j<=m; j++)
+            {
+                cin >> a[i][j];
+            }
+        }
+        int ans = 0;
+        int f[501][501] = {};
+        for (int i=1; i<=n; i++)
+        {
+            for (int j=1; j<=m; j++)
+            {
+                if (a[i][j] == 0)
+                {
+                    f[i][j] = 0;
+                }
+                else f[i][j] = min({f[i-1][j],f[i-1][j-1],f[i][j-1]}) + 1;
+                ans = max(ans,f[i][j]);
+            }
+        }
+        cout << ans << endl;
+    }
+    return 0;
+}
