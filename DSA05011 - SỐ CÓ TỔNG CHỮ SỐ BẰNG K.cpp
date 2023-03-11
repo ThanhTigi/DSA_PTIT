122580024945cd9d7428f8bf45d90c0bf1d043e0
Thành <118548931+ThanhTigi@users.noreply.github.com>
2023-03-11 19:57:56 +0700

    Add files via upload

---
+++ DSA05011 - SỐ CÓ TỔNG CHỮ SỐ BẰNG K.cpp
@@ -0,0 +1,24 @@
+// DSA_PTIT By Thanh TiGi
+#include <bits/stdc++.h>
+using namespace std;
+
+const int mod = 1e9 + 7;
+
+int main()
+{
+    int t, n, k;
+    cin >> t;
+    int dp[105][50005] = {};
+    for(int i = 1; i <= 9; ++i)
+        dp[1][i] = 1;
+    for(int i = 1; i <= 100; ++i)
+        for(int j = 0; j <= 9; ++j)
+            for(int z = j; z <= 50000; ++z)
+                dp[i][z] = (dp[i][z] + dp[i - 1][z - j]) % mod;
+    while(t--)
+    {
+        cin >> n >> k;
+        cout << dp[n][k] << endl;
+    }
+    return 0;
+}
